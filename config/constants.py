2a5268572aee08ae2e50a73c53323197d8c6b2af
Adrian Castro <70278376+kastro0005@users.noreply.github.com>
2025-09-15 17:15:58 -0400

    ALS2, BLS ER added

--- config/constants.py
+++ config/constants.py
@@ -5,8 +5,10 @@ LEVEL_OF_SERVICE_BASE_RATES = {
     "WCH": 100,   # Silla de ruedas
     "STR": 175,  # Camilla
     "BLS": 450,   # Soporte vital básico
+    "BLS ER": 500, # Soporte vital básico con especialista
     "ALS NE": 550,   # Soporte vital avanzado
     "ALS ER": 600,   # Soporte vital avanzado con especialista
+    "ALS2": 800,  # Soporte vital avanzado nivel 2
     "CCT": 950,   # Transporte de cuidado critico
 
     
@@ -17,8 +19,10 @@ LEVEL_OF_SERVICE_BASE_RATES_BAPTIST = {
     "WCH": 80,   # Silla de ruedas
     "STR": 100,  # Camilla
     "BLS": 450,   # Soporte vital básico
+    "BLS ER": 500, # Soporte vital básico con especialista
     "ALS NE": 550,   # Soporte vital avanzado
     "ALS ER": 600,   # Soporte vital avanzado con especialista
+    "ALS2": 800,  # Soporte vital avanzado nivel 2
     "CCT": 950,   # Transporte de cuidado critico
     
 }
@@ -28,8 +32,10 @@ LEVEL_OF_SERVICE_BASE_RATES_HCA = {
     "WCH": 70,   # Silla de ruedas
     "STR": 105,  # Camilla
     "BLS": 450,   # Soporte vital básico
+    "BLS ER": 500, # Soporte vital básico con especialista
     "ALS NE": 550,   # Soporte vital avanzado
     "ALS ER": 600,   # Soporte vital avanzado con especialista
+    "ALS2": 800,  # Soporte vital avanzado nivel 2
     "CCT": 950,   # Transporte de cuidado critico
     
 }
@@ -44,8 +50,10 @@ LEVEL_OF_SERVICE_INCREMENTS = {
     "WCH": 5,
     "STR": 9,
     "BLS": 11.5,
+    "BLS ER": 11.5, # Soporte vital básico con especialista
     "ALS NE": 11.5 ,
     "ALS ER": 11.5 ,  
+    "ALS2": 11.5,  # Soporte vital avanzado nivel 2
     "CCT": 11.5 , 
 }
 
@@ -65,10 +73,17 @@ WAITING_TIME_RATE = {
     "WCH": 50,
     "STR": 75,
     "BLS": 100,
+    "BLS ER": 500, # Soporte vital básico con especialista
     "ALS NE": 150,
     "ALS ER": 600,
+    "ALS2": 800,  # Soporte vital avanzado nivel 2
     "CCT": 200,
 } 
+
+
+"""Constantes para la parte de geolocalización y cálculo de rutas
+   realmente no se usan demasiados y no son tan im[portantes pero quizas mada delnate si]"""
+
 
 # Configuración de geolocalización
 MAX_RETRIES = 3
